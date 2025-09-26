0b2fcff6dfe89dab6b2d95b5686213d01a4b7117
Mahesh7s <mokamahesh77@gmail.com>
2025-09-26 10:34:14 +0530

    commiting

--- routes/transactionRoutes.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const tRouter = express.Router()
-const transactionController = require("../controllers/transactionController")
-tRouter.post("/add",transactionController.addTransaction);
-module.exports=tRouter;
