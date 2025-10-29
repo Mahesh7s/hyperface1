41e6b9ab6ca634e4710bd1d117a2c650f638a37f
Mahesh7s <mokamahesh77@gmail.com>
2025-10-29 18:13:38 +0530

    commiting the new code

---
+++ Rev/bug3/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ProfileSearch from './ProfileSearch'
+
+function App() {
+   
+  return (
+    <>
+       <ProfileSearch/>
+    </>
+  )
+}
+
+export default App
